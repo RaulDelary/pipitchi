8e3f642a799a0d402492e5556ffa5f1cf545ce1d
Raul Delary <rauldelary@gmail.com>
2022-12-14 05:00:35 -0300

    radical changes and low quality code used to make some kind of UI

---
+++ src/cli.py
@@ -0,0 +1,177 @@
+import sys
+from datetime import datetime
+from openpyxl import load_workbook
+from emoji import emojize
+from requests import post
+from json import dumps
+from configparser import ConfigParser
+
+global ms_teams_webhook
+ms_teams_webhook = None
+
+
+def __conditional_emoji__ (rule, value = 0):
+  match rule:
+    case 1:
+      return ':check_mark_button:' if value <= 79 else ':warning:' if value > 79 and value <= 90 else ':hollow_red_circle:'
+
+    case 2:
+      return ':hollow_red_circle:' if value <= 80 else ':warning:' if value > 80 and value <= 90 else ':check_mark_button:'
+
+    case 3:
+      return ':hollow_red_circle:' if value <= 10 else ':warning:' if value > 10 and value <= 19 else ':check_mark_button:'
+
+    case 4:
+      return ':check_mark_button:' if value >= 95 else ':warning:'
+
+    case 5:
+      return ':hollow_red_circle:' if 'Falha' in value else ':warning:' if 'Parcial' in value else ':check_mark_button:'
+
+def __conditional_string__ (rule, value = 0):
+  match rule:
+    case 1:
+      return 'Crescimento/dia' if value >= 0 else 'Redução nas últimas 24h'
+    
+    case 2:
+      date_str = value.split ('\n') [0].split () [2]
+      return f'{date_str [8:]}/{date_str [5:7]}/{date_str [0:4]}'
+
+def format_message (ws):
+  msg_template = '''
+{em01} Health-Check Backup
+{em02} {data_hora}
+
+{em03} Infraestrutura
+(AVAMAR E VEEAM)
+
+RJ1
+  {em04} Área Ocupada: {area_ocupada_rj1}%
+  {em05} Área Livre: {area_livre_rj1} TB
+  {em06} {cres_dec_str_rj1}: {cres_dec_value_rj1} TB
+
+RJ2
+  {em07} Área Ocupada: {area_ocupada_rj2}%
+  {em08} Área Livre: {area_livre_rj2} TB
+  {em09} {cres_dec_str_rj2}: {cres_dec_value_rj2} TB
+
+{em10} Replicação AWS
+  {em11} RJ1 Executado: {rep_aws_executado_rj1}%
+  {em12} Dt última: {rep_aws_ultima_rj1}
+  {em13} Área Livre: {rep_aws_livre_rj1}%
+
+  {em14} RJ2 executado: {rep_aws_executado_rj2}%
+  {em15} Dt última: {rep_aws_ultima_rj2}
+  {em16} Área Livre: {rep_aws_livre_rj2}%
+
+
+{em17} Execuções Data Center
+  {em18} JOBS RJ1: {jobs_rj1}
+  {em19} Executado: {jobs_executado_rj1:.2f}% 
+
+  {em20} JOBS RJ2: {jobs_rj2}
+  {em21} Executado: {jobs_executado_rj2:.2f}%
+
+
+{em22} Execuções Unidades
+  {em23} Escopo: {count_units} Locais
+  {em24} Sucesso: {pct_sucesso_total:.2f}%
+'''
+
+  status_column = tuple (ws.iter_cols (min_col = 4, max_col = 4, min_row = 8, values_only = True)) [0]
+  count_units = len (status_column)
+  count_valid_units = len ([x for x in status_column if x != 'Sem backup no dia'])
+  count_success = len ([x for x in status_column if x == 'Sucesso'])
+  unit_success_pct = (count_success / count_units) * 100
+
+  return msg_template.format (
+  em01 = ':stethoscope:',
+  em02 = ':spiral_calendar:', data_hora = datetime.now ().strftime ('%d/%m/%Y'),
+
+  em03 = ':large_blue_diamond:',
+  em04 = __conditional_emoji__ (1, ws ['O3'].value * 100), area_ocupada_rj1 = int (ws ['O3'].value * 100),
+  em05 = ':information:', area_livre_rj1 = ws ['I3'].value,
+  em06 = ':information:', cres_dec_str_rj1 = __conditional_string__ (1, ws ['J3'].value), cres_dec_value_rj1 = ws ['J3'].value,
+
+  em07 = __conditional_emoji__ (1, ws ['O4'].value * 100), area_ocupada_rj2 = int (ws ['O4'].value * 100),
+  em08 = ':information:', area_livre_rj2 = ws ['I4'].value,
+  em09 = ':information:', cres_dec_str_rj2 = __conditional_string__ (1, ws ['J4'].value), cres_dec_value_rj2 = ws ['J4'].value,
+
+  em10 = ':large_blue_diamond:',
+  em11 = __conditional_emoji__ (2, ws ['G3'].value * 100), rep_aws_executado_rj1 = ws ['G3'].value * 100,
+  em12 = ':information:', rep_aws_ultima_rj1 = __conditional_string__ (2, ws ['H3'].value),
+  em13 = __conditional_emoji__ (3, ws ['K3'].value), rep_aws_livre_rj1 = ws ['K3'].value,
+
+  em14 = __conditional_emoji__ (2, ws ['G4'].value * 100), rep_aws_executado_rj2 = ws ['G4'].value * 100,
+  em15 = ':information:', rep_aws_ultima_rj2 = __conditional_string__ (2, ws ['H4'].value),
+  em16 = __conditional_emoji__ (3, ws ['K4'].value), rep_aws_livre_rj2 = ws ['K4'].value,
+
+  em17 = ':large_blue_diamond:',
+  em18 = ':information:', jobs_rj1 = ws ['N3'].value,
+  em19 = __conditional_emoji__ (4, ws ['E3'].value * 100), jobs_executado_rj1 = ws ['E3'].value * 100,
+
+  em20 = ':information:', jobs_rj2 = ws ['N4'].value,
+  em21 = __conditional_emoji__ (4, ws ['E4'].value * 100), jobs_executado_rj2 = ws ['E4'].value * 100,
+
+  em22 = ':large_blue_diamond:',
+  em23 = __conditional_emoji__ (5, status_column), count_units = count_units,
+  em24 = ':information:', pct_sucesso_total = unit_success_pct
+)
+
+def send_teams_message (message):
+  if not ms_teams_webhook: return
+  
+  payload = {'text': message}
+  response = post (ms_teams_webhook, data = dumps (payload), timeout = 10)
+
+  if (response.status_code != 200):
+    raise Exception ('Status Code da requisição diferente de 200.')
+
+def open_wb (file_name): # Retorna Worksheet
+  try:
+    wb = load_workbook (filename = file_name, data_only = True)
+  
+  except FileNotFoundError:
+    return f'Não foi possível abrir o arquivo {file_name}. Verifique se o caminho do arquivo está correto.'
+
+  return wb
+
+def cli ():
+  try:
+    file_name = sys.argv [1]
+    wb = load_workbook (filename = file_name, data_only = True)
+
+  except FileNotFoundError:
+    print (f'Não foi possível abrir o arquivo {file_name}. Verifique se o caminho do arquivo está correto.')
+    exit (1)
+
+  except IndexError:
+    # To Do: Implement usage function.
+    print (f'Nome do arquivo de entrada não foi informado.')
+    exit (1)
+
+  ws = wb.active
+
+  output_text = format_message (ws)
+  emojized_output = emojize (output_text)
+
+  print (emojized_output)
+
+  try:
+    send_teams_message (emojized_output)
+  except:
+    print ('Não foi possível utilizar o webhook. Verifique se o endpoint está correto ou verifique as suas configurações de rede.')
+
+
+try:
+  configs = ConfigParser ()
+  configs.read (filenames = './app.ini', encoding = 'UTF-8')
+
+  ms_teams_webhook = configs ['connectors']['teams_webhook']
+
+except KeyError:
+  print (f'Não foi possível carregar o arquivo de configuração. Webhook do Teams não será utilizado')
+
+
+
+if __name__ == '__main__':
+  cli ()
---
+++ src/ui.py
@@ -0,0 +1,141 @@
+from tkinter import filedialog as fd, Tk, Button, Label, Text, DISABLED, NORMAL, END
+import threading
+import cli
+
+root = Tk ()
+root.title (u'\U0001f4f0  Exporting Datasheet Healthcheck Info  	\U0001fa7a')
+root.resizable (False, False)
+
+screen_width = root.winfo_screenwidth () # + 3500
+screen_height = root.winfo_screenheight ()
+root_width = 500
+root_height = 600
+root.geometry (f'{root_width}x{root_height}+{int ((screen_width / 2) - (root_width / 2))}+{int ((screen_height / 2) - (root_height / 2))}')
+
+
+def main_btn_event_handler ():
+  file_name = fd.askopenfilename (defaultextension = '.xlsx')
+
+  if not file_name:
+    feedback_label.config (text = 'Erro desconhecido')
+    return
+  else:
+    feedback_label.config (text = 'Iniciando Extração das Informações...')
+  
+  def run_sub ():
+    try:
+      wb = cli.open_wb (file_name)
+      output_text = cli.format_message (wb.active)
+      emojized_output = cli.emojize (output_text)
+    
+    except FileNotFoundError:
+      feedback_label.config (text = 'Erro! Não foi possível abrir o arquivo.')
+
+    except:
+      feedback_label.config (text = 'Erro desconhecido.')
+    
+    else:
+      feedback_label.config (text = 'Healthcheck Gerado com Sucesso!!')
+
+
+    text_box_output.delete ('1.0', END)
+    text_box_output.insert (END, emojized_output)
+  
+  threading.Thread (target = run_sub).start ()
+
+def send_msg_btn_event_handler ():
+  def run_sub ():
+    try:
+      feedback_label.config (text = 'Enviando mensagem...')
+      cli.send_teams_message (text_box_output.get ('1.0', END))
+      feedback_label.config (text = 'Mensagem enviada!')
+
+    except:
+      feedback_label.config (text = 'Erro ao enviar mensagem ao Teams.\nVerifique sua URL do Webhook ou suas configurações de rede.')
+
+  threading.Thread (target = run_sub).start ()
+
+def copy_btn_event_handler ():
+  try:
+    if len (text_box_output.get ('1.0', END)) == 1:
+      feedback_label.config (text = 'Não há nada para ser copiado!')
+      return
+    
+    root.clipboard_clear ()
+    root.clipboard_append (text_box_output.get ('1.0', END))
+
+  except:
+    feedback_label.config (text = 'Erro ao copiar para a área de transferência.')
+
+  else:
+    feedback_label.config (text = 'Mensagem copiada!!')
+  
+def quit_btn_event_handler ():
+  root.destroy ()
+
+
+
+
+main_btn = Button (
+  root,
+  text = 'Selecionar Planilha',
+  width = 65,
+  height = 2,
+  command = main_btn_event_handler
+)
+main_btn.place (x = 15, y = 60)
+main_btn.pack (pady = 20)
+
+
+feedback_label = Label (
+  root,
+  text = ''
+)
+feedback_label.pack (pady = 20)
+
+
+text_box_output = Text (
+  root,
+  width = 57,
+  height = 20
+)
+text_box_output.place (x = 15, y = 150)
+
+
+send_msg_btn = Button (
+  root,
+  text = 'Enviar Mensagem Teams',
+  width = 30,
+  height = 2,
+  state = DISABLED,
+  command = send_msg_btn_event_handler
+)
+send_msg_btn.place (x = 15, y = 490)
+
+if cli.ms_teams_webhook:
+  send_msg_btn ['state'] = NORMAL
+else:
+  feedback_label.config (text = 'O envio de mensagens ao Teams está desabilitado.\nNão foi possível ler o arquivo de configuração.')
+
+
+copy_btn = Button (
+  root,
+  text = 'Copiar',
+  width = 30,
+  height = 2,
+  command = copy_btn_event_handler
+)
+copy_btn.place (x = 255, y = 490)
+
+
+quit_btn = Button (
+  root,
+  text = 'Sair',
+  width = 30,
+  height = 2,
+  command = quit_btn_event_handler
+)
+quit_btn.place (x = 15, y = 545)
+
+
+root.mainloop ()
